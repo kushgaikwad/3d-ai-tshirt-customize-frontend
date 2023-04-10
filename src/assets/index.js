93a8d3547cd7b14a038f00edbd54221ca1540385
Kush <kushalxgaikwad@gmail.com>
2023-04-10 11:51:55 +0530

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import ai from "./ai-removebg-preview.png";
+import fileIcon from "./file_upload-removebg-preview.png";
+import swatch from "./swatch.png";
+import download from "./download.png";
+
+import logoShirt from "./logo-tshirt.png";
+import stylishShirt from "./stylish-tshirt.png";
+
+export { ai, fileIcon, swatch, download, logoShirt, stylishShirt };
